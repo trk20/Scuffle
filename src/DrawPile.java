7947a6ae4fcced738edeccb4df49ca172798cd77
orselane <alexandremarques@cmail.carleton.ca>
2022-10-22 14:12:13 -0400

    Created Alex M1 classes
    -Includes: Hand, DrawPile, Letter (already partially done)
    -Names, + class docstring

---
+++ src/DrawPile.java
@@ -0,0 +1,10 @@
+/**
+ * Class DrawPile takes care of the letter drawing logic.
+ * Keeps track of drawn letters to reduce their draw likelihoods.
+ *
+ * @author Alexandre Marques - 101189743
+ * @version 2022-10-22
+ */
+public class DrawPile {
+    //TODO: Implement class
+}
---
+++ src/Hand.java
@@ -0,0 +1,12 @@
+/**
+ * Class Hand takes care of holding Letter tiles for a Player.
+ * Keeps track of letters currently help by the Player,
+ * checking if the Player has the Letter(s) to play a word,
+ * getting new letters from the DrawPile.
+ *
+ * @author Alexandre Marques - 101189743
+ * @version 2022-10-22
+ */
+public class Hand {
+    //TODO: Implement class
+}
