023e53b5c0d68b322cbe60dc885e3fe3fe29ebe9
Timothy Kennedy <70790401+trk20@users.noreply.github.com>
2022-10-21 22:21:11 -0400

    Removed duplicate letter scores

--- src/Board.java
+++ src/Board.java
@@ -17,35 +17,6 @@ public class Board {
     private BoardTile[][] board;
     private String[] column = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"};
 
-    //letter values for scoring
-    private HashMap<Character,Integer> letterValues = new HashMap<Character, Integer>(){{
-        put('A',1);
-        put('E',1);
-        put('I',1);
-        put('L',1);
-        put('N',1);
-        put('O',1);
-        put('R',1);
-        put('S',1);
-        put('T',1);
-        put('U',1);
-        put('D',2);
-        put('G',2);
-        put('B',3);
-        put('C',3);
-        put('M',3);
-        put('P',3);
-        put('F',4);
-        put('H',4);
-        put('V',4);
-        put('W',4);
-        put('Y',4);
-        put('K',5);
-        put('J',8);
-        put('X',8);
-        put('Z',10);
-    }};
-
     private DictionaryHandler dictionary;
 
     public Board(int length, int width){
