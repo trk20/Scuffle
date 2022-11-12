16bd7595e3b37ec7389545bad4fd82f0ab10ed9d
Vladimir Kovacina <vladimirkovacina@yahoo.com>
2022-11-12 11:19:26 -0500

    Fixing Issues discovered in the tests for BoardTile

--- src/Model/BoardTile.java
+++ src/Model/BoardTile.java
@@ -1,11 +1,13 @@
 package Model;
 
 
+import java.util.Objects;
+
 /**
  * A class handling the board tiles,
  *
  * @author Vladimir Kovacina
- * @version 1.0
+ * @version 1.2
  *
  * @author Timothy Kennedy
  * @version 1.1
@@ -89,8 +91,10 @@ public class BoardTile {
      * @param letter the letter the tile now has
      */
     public void setLetter(Letter letter){
-        this.letter = letter; // FIXME: Can override letters!
-        taken = true;
+        if(!taken) {
+            this.letter = letter;
+            taken = true;
+        }
     }
 
     /**
@@ -138,4 +142,13 @@ public class BoardTile {
         }
         return tileType.toString().replaceAll("BLANK"," -- ").replace("START", "ST").replace("X2WORD", "2W").replace("X3WORD", "3W").replace("X2LETTER", "2L").replace("X3LETTER", "3L");
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BoardTile boardTile = (BoardTile) o;
+        return letter == boardTile.letter && tileType == boardTile.tileType;
+    }
+
 }
