60ee7337cd0ce68b92c6c6427fce4d735319582a
trken <trkennedy01@gmail.com>
2022-11-29 23:34:38 -0500

    -Changed AI algorithm to rely much less on randomness -Added delay on AI actions so they can be player viewable

--- src/Model/DictionaryHandler.java
+++ src/Model/DictionaryHandler.java
@@ -1,24 +1,26 @@
 package Model;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
 
 /**
  * A class handling the loading of the set of valid words, and the evaluation of the validity of possible words
  *
  * @author Timothy Kennedy
- * @version 1.1
+ * @version NOV-29
  */
 public class DictionaryHandler {
     private final ArrayList<String> allWords; // Storage for all valid words
+    private final HashMap<String,HashMap<String,Integer>> letterCounts;
+
 
     /**
-     * Constructs a new Model.DictionaryHandler object with all valid words loaded into an ArrayList
+     * Constructs a new Model.DictionaryHandler object with all valid words loaded into an ArrayList, and a HashMap of their letter frequencies.
      */
     public DictionaryHandler() {
         allWords = new ArrayList<>();
+        letterCounts = new HashMap<>();
         /*
          * Load all valid words from the 2019 version of Scrabble into an ArrayList
          * If something goes wrong, print an error message
@@ -27,14 +29,23 @@ public class DictionaryHandler {
             File dictionary = new File("Collins Scrabble Words (2019).txt");
             Scanner scanner = new Scanner(dictionary);
             while (scanner.hasNextLine()) {
-                allWords.add(scanner.nextLine());
+                String word = scanner.nextLine();
+                allWords.add(word);
             }
             scanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("Something went wrong with loading the dictionary");
-
         }
 
+        //create Hashmap of words and their associated letter frequencies
+        allWords.parallelStream().forEach(word->{
+            HashMap<String, Integer> letterCount = new HashMap<>();
+            IntStream distinct = word.chars().distinct();
+            for (int letter : distinct.toArray()) {
+                letterCount.put(Character.toString(letter), (int) word.chars().filter(e -> e == letter).count());
+            }
+            letterCounts.put(word, letterCount);
+        });
 
     }
 
@@ -46,5 +57,22 @@ public class DictionaryHandler {
      */
     public boolean isValidWord(String maybeAWord){
         return allWords.contains(maybeAWord.toUpperCase());
+    }
+
+    /**
+     * Find all dictionary words that match the letter frequencies passed to it
+     *
+     * @param letterCount the letter frequencies to consider
+     * @return a list of all valid words with those letters
+     */
+    public ArrayList<ArrayList<String>> getValidWords(HashMap<String,Integer> letterCount){
+        ArrayList<ArrayList<String>> words = new ArrayList<>();
+        letterCounts.entrySet().stream()
+                .filter(entry -> letterCount.equals(entry.getValue()))
+                .map(Map.Entry::getKey)
+                .forEach(
+                        word->words.add(new ArrayList<>(Arrays.stream(word.split("")).toList()))
+                );
+        return words;
     }
 }
